cfb71d494d7d352b66f3aba126a5f04e68b43a03
kou-hc <konstantin0780@gmail.com>
2022-03-02 17:11:52 +0300

    Added data binding, click handlers, view model and basic functionality.

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -19,13 +19,117 @@ package com.example.android.trackmysleepquality.sleeptracker
 import android.app.Application
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.formatNights
+import kotlinx.coroutines.*
 
 /**
  * ViewModel for SleepTrackerFragment.
  */
 class SleepTrackerViewModel(
-        val database: SleepDatabaseDao,
-        application: Application) : AndroidViewModel(application) {
+    val database: SleepDatabaseDao,
+    application: Application
+) : AndroidViewModel(application) {
+
+        private var viewModelJob = Job()
+
+        private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+        private var tonight = MutableLiveData<SleepNight?>()
+
+        private val nights = database.getAllNights()
+
+        val nightsString = Transformations.map(nights) { nights ->
+                formatNights(nights, application.resources)
+        }
+
+        init {
+                initializeTonight()
+        }
+
+        /**
+         * Using the UI scope coroutine because:
+         * 1. We need to use non-blocking suspend fun to prevent
+         *      UI from freezing.
+         * 2. We do some work that affects the UI ->
+         *      we need to use UI scope
+         */
+        private fun initializeTonight() {
+                uiScope.launch {
+                        tonight.value = getTonightFromDatabase()
+                }
+        }
+
+        /**
+         * Using the IO dispatcher because:
+         * 1. We don't have work that affect the UI and
+         *      we don't want to overload UI with long
+         *      operations.
+         * 2. We want to use thread pool from dispatcher
+         *      that is optimized for IO operations.
+         */
+        private suspend fun getTonightFromDatabase(): SleepNight? {
+                return withContext(Dispatchers.IO) {
+                        var night = database.getTonight()
+                        if (night?.endTimeMilli != night?.startTimeMilli) {
+                                night = null
+                        }
+                        night
+                }
+        }
+
+        fun onStartTracking() {
+                uiScope.launch {
+                        val newNight = SleepNight()
+
+                        insert(newNight)
+
+                        tonight.value = getTonightFromDatabase()
+                }
+        }
+
+        private suspend fun insert(night: SleepNight) {
+                withContext(Dispatchers.IO) {
+                        database.insert(night)
+                }
+        }
+
+        fun onStopTracking() {
+                uiScope.launch {
+                        val oldNight = tonight.value ?: return@launch
+
+                        oldNight.endTimeMilli = System.currentTimeMillis()
+
+                        update(oldNight)
+                }
+        }
+
+        private suspend fun update(night: SleepNight) {
+                withContext(Dispatchers.IO) {
+                        database.update(night)
+                }
+        }
+
+        fun onClear() {
+                uiScope.launch {
+                        clear()
+
+                        tonight.value = null
+                }
+        }
+
+        private suspend fun clear() {
+                withContext(Dispatchers.IO) {
+                        database.clear()
+                }
+        }
+
+        override fun onCleared() {
+                super.onCleared()
+                viewModelJob.cancel()
+        }
 }
 
